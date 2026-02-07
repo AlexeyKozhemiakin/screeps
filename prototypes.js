e46d122004bcca06ce304d352d755547249667b3
Alexey <alexey.kozhemiakin@gmail.com>
2026-02-07 11:07:24 +0100

    refactor: improve code formatting and consistency in prototypes.js
    - Standardize whitespace and indentation throughout
    - Fix spacing in function declarations and object properties
    - Improve readability of filter functions and property definitions
    - Add consistent spacing around operators and braces

--- prototypes.js
+++ prototypes.js
@@ -1,223 +1,227 @@
 // Structure Prototypes
-StructureContainer.prototype.isOperating = function()
-{ 
-    var dels = _.filter(Game.creeps,  d => d.memory.role == "deliverer" && d.memory.preferredTargetId == this.id); 
-    var dels2 = _.filter(Game.creeps,  d => d.memory.role == "deliverer" && d.memory.preferredSourceId == this.id); 
+StructureContainer.prototype.isOperating = function () {
+    var dels = _.filter(Game.creeps, d => d.memory.role == "deliverer" && d.memory.preferredTargetId == this.id);
+    var dels2 = _.filter(Game.creeps, d => d.memory.role == "deliverer" && d.memory.preferredSourceId == this.id);
     var value = dels.length > 0 || dels2.length > 0
 
     return value || this.store[RESOURCE_ENERGY] > 1000;
 };
 
-StructureLink.prototype.isOperating = function()
-{ 
+StructureLink.prototype.isOperating = function () {
     return true;
 };
 
-StructureStorage.prototype.isOperating = function()
-{ 
+StructureStorage.prototype.isOperating = function () {
     return true;
 };
-    
-Source.prototype.isOperating = function()
-{ 
-    var dels = _.filter(Game.creeps,  d => d.memory.role == "harvester" && d.memory.preferredSourceId == this.id); 
-    
+
+Source.prototype.isOperating = function () {
+    var dels = _.filter(Game.creeps, d => d.memory.role == "harvester" && d.memory.preferredSourceId == this.id);
+
     return dels.length > 0;
 };
 
-Source.prototype.slots = function()
-{ 
-    var array = this.room.lookForAtArea(LOOK_TERRAIN, this.pos.y - 1, this.pos.x -1, this.pos.y + 1, this.pos.x + 1, true);
-    
+Source.prototype.slots = function () {
+    var array = this.room.lookForAtArea(LOOK_TERRAIN, this.pos.y - 1, this.pos.x - 1, this.pos.y + 1, this.pos.x + 1, true);
+
     var len = _.filter(array, p => p.terrain != 'wall').length;
-    
+
     return len;
 };
 
 // Room Prototypes
 Object.defineProperty(Room.prototype, 'extractor', {
-  get: function() 
- {
-     return  this.find(FIND_STRUCTURES, {filter : {structureType : STRUCTURE_EXTRACTOR}})
+    get: function () {
+        return this.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_EXTRACTOR } })
         [0];
- },
- 
- enumerable: false,
- configurable: true
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(RoomObject.prototype, 'isNearBase', {
- get: function() 
- {
-     return  this.pos.findInRange(FIND_MY_STRUCTURES, 2, {filter: s=> (s.structureType == STRUCTURE_EXTENSION || 
-        s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_STORAGE) }).length > 0;
- },
- 
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.pos.findInRange(FIND_MY_STRUCTURES, 2, {
+            filter: s => (s.structureType == STRUCTURE_EXTENSION ||
+                s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_STORAGE)
+        }).length > 0;
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'links', {
- get: function() 
- {
-     return  this.find(FIND_STRUCTURES, {filter : {structureType : STRUCTURE_LINK}});
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_LINK } });
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'extensions', {
- get: function() 
- {
-     return  this.find(FIND_STRUCTURES, {filter : {structureType : STRUCTURE_EXTENSION}});
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION } });
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'spawn', {
- get: function() 
- {
-     return  this.spawns[0];
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.spawns[0];
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'spawns', {
- get: function() 
- {
-     return  this.find(FIND_MY_STRUCTURES, {filter : {structureType : STRUCTURE_SPAWN}});
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'towers', {
- get: function() 
- {
-     return  this.find(FIND_MY_STRUCTURES, {filter : {structureType : STRUCTURE_TOWER}});
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        return this.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'config', {
- get: function() 
- {
-    if(!Memory.rooms[this.name].config)
-        Memory.rooms[this.name].config = new Object();
-        
-    return Memory.rooms[this.name].config;
- },
- 
- set: function(conf) 
- {
-    return Memory.rooms[this.name].config = conf;
- },
- enumerable: false,
- configurable: true
+    get: function () {
+        if (!Memory.rooms[this.name].config)
+            Memory.rooms[this.name].config = new Object();
+
+        return Memory.rooms[this.name].config;
+    },
+
+    set: function (conf) {
+        return Memory.rooms[this.name].config = conf;
+    },
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(RoomObject.prototype, 'container', {
- get: function() 
- {
-    var closest = this.pos.findInRange(FIND_STRUCTURES, 2, {filter: {structureType: STRUCTURE_CONTAINER}})[0]
-    
-    if(closest == undefined)
-        closest = this.pos.findInRange(FIND_STRUCTURES, 3, {filter: {structureType: STRUCTURE_CONTAINER}})[0]
-   
-     if(closest == undefined)
-        closest = this.pos.findInRange(FIND_STRUCTURES, 4, {filter: {structureType: STRUCTURE_CONTAINER}})[0]
-   
-    return closest
- },
- 
- enumerable: false,
- configurable: true
+    get: function () {
+        var closest = this.pos.findInRange(FIND_STRUCTURES, 2, { filter: { structureType: STRUCTURE_CONTAINER } })[0]
+
+        if (closest == undefined)
+            closest = this.pos.findInRange(FIND_STRUCTURES, 3, { filter: { structureType: STRUCTURE_CONTAINER } })[0]
+
+        if (closest == undefined)
+            closest = this.pos.findInRange(FIND_STRUCTURES, 4, { filter: { structureType: STRUCTURE_CONTAINER } })[0]
+
+        return closest
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(RoomObject.prototype, 'storage', {
- get: function() 
- {
-    return  this.pos.findInRange(FIND_STRUCTURES, 2, {filter: {structureType: STRUCTURE_STORAGE}})
+    get: function () {
+        return this.pos.findInRange(FIND_STRUCTURES, 2, { filter: { structureType: STRUCTURE_STORAGE } })
         [0];
- },
- 
- enumerable: false,
- configurable: true
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'mineral', {
- 
- get: function() 
- {
-    return  this.find(FIND_MINERALS)  [0];
- },
- 
- enumerable: false,
- configurable: true
+
+    get: function () {
+        return this.find(FIND_MINERALS)[0];
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
 Object.defineProperty(Room.prototype, 'basecontainer', {
- 
- get: function() 
- {
-    return  this.spawn.pos.findInRange(FIND_STRUCTURES, 4, {filter: {structureType: STRUCTURE_CONTAINER}})
+
+    get: function () {
+        return this.spawn.pos.findInRange(FIND_STRUCTURES, 4, { filter: { structureType: STRUCTURE_CONTAINER } })
         [0];
- },
- 
- enumerable: false,
- configurable: true
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
-Object.defineProperty(RoomObject.prototype, 'link', {
- 
- get: function() 
- {
-    return  this.pos.findInRange(FIND_STRUCTURES, 4, {filter: {structureType: STRUCTURE_LINK}})
+
+Object.defineProperty(StructureController.prototype, "storage", {
+
+    get: function () {
+        return this.pos.findInRange(FIND_STRUCTURES, 4, { filter: { structureType: STRUCTURE_STORAGE } })
         [0];
- },
- 
- enumerable: false,
- configurable: true
+    },
+
+    enumerable: false,
+    configurable: true
 });
 
+Object.defineProperty(StructureController.prototype, "link", {
+
+    get: function () {
+        return this.pos.findInRange(FIND_STRUCTURES, 4, { filter: { structureType: STRUCTURE_LINK } })
+        [0];
+    },
+
+    enumerable: false,
+    configurable: true
+});
+
+Object.defineProperty(RoomObject.prototype, "link", {
+
+    get: function () {
+        return this.pos.findInRange(FIND_STRUCTURES, 2, { filter: { structureType: STRUCTURE_LINK } })
+        [0];
+    },
+
+    enumerable: false,
+    configurable: true
+});
 
 Object.defineProperty(RoomObject.prototype, 'mineralDemand', {
- 
- get: function() 
- {
-   if(this.id == "5b17ead465803e4ceedf4ce3")
-    return RESOURCE_HYDROGEN;
-    
-   if(this.id == "5b1805832bee550ad3d83114")
-    return RESOURCE_OXYGEN;
+
+    get: function () {
+        if (this.id == "5b17ead465803e4ceedf4ce3")
+            return RESOURCE_HYDROGEN;
+
+        if (this.id == "5b1805832bee550ad3d83114")
+            return RESOURCE_OXYGEN;
 
 
-    if(this.id == '5b2d2822e8d5d019c1d778dd')
-        return RESOURCE_KEANIUM;
-    if(this.id == '5b2e22f3d08a6b242e80f462')
-        return RESOURCE_ZYNTHIUM;
-    
-    if(this.id == '5b29a59e8d6e3c5e7f198aa2')
-        return RESOURCE_GHODIUM;
-    if(this.id == '5b192f05c9146e7a0e5721a5')   
-        return RESOURCE_HYDROGEN;
-        
-    if(this.id == '5b17d4f32d8da349cebafd64')
-        return RESOURCE_UTRIUM;
-    if(this.id == '5b182ff94226844ce49e4492')   
-        return RESOURCE_LEMERGIUM;   
-        
-    if(this.id == '5b2db26618d810244ad0119b')
-        return RESOURCE_UTRIUM_LEMERGITE;
-    if(this.id == '5b2d97788002ae26413f2e78')   
-        return RESOURCE_ZYNTHIUM_KEANITE;       
-        
-   return undefined;
- },
- 
- enumerable: false,
- configurable: true
+        if (this.id == '5b2d2822e8d5d019c1d778dd')
+            return RESOURCE_KEANIUM;
+        if (this.id == '5b2e22f3d08a6b242e80f462')
+            return RESOURCE_ZYNTHIUM;
+
+        if (this.id == '5b29a59e8d6e3c5e7f198aa2')
+            return RESOURCE_GHODIUM;
+        if (this.id == '5b192f05c9146e7a0e5721a5')
+            return RESOURCE_HYDROGEN;
+
+        if (this.id == '5b17d4f32d8da349cebafd64')
+            return RESOURCE_UTRIUM;
+        if (this.id == '5b182ff94226844ce49e4492')
+            return RESOURCE_LEMERGIUM;
+
+        if (this.id == '5b2db26618d810244ad0119b')
+            return RESOURCE_UTRIUM_LEMERGITE;
+        if (this.id == '5b2d97788002ae26413f2e78')
+            return RESOURCE_ZYNTHIUM_KEANITE;
+
+        return undefined;
+    },
+
+    enumerable: false,
+    configurable: true
 });
